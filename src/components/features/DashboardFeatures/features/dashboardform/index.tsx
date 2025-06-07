73f33bc2ca141d64002601528c0d19416934c949
MRnektodotpng <android.kaplan@gmail.com>
2025-06-07 19:36:05 +0200

    updated dashboard and back navigation

--- src/components/features/DashboardFeatures/features/dashboardform/index.tsx
+++ src/components/features/DashboardFeatures/features/dashboardform/index.tsx
@@ -5,6 +5,7 @@ import { deleteCard } from "../../../../../utils/card/card";
 import { FinancialOverview } from "../../../../atoms/FinancialOverview";
 import { API_URL } from "../../../../api/baseUrl";
 import { useNavigate } from "react-router-dom";
+import { LoggedNavBar } from "../../../../shared/LoggedNavBar";
 
 type CardData = {
   number: string;
@@ -16,11 +17,20 @@ type CardData = {
   design: string;
 };
 
+
 export const DashboardForm = () => {
   const [cardData, setCardData] = useState<CardData[] | null>(null);
   const [selectedCard, setSelectedCard] = useState<CardData | null>(null);
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [screenSize, setScreenSize] = useState<number>(0)
   const navigate = useNavigate();
+
+  const screenResize = () => {
+    setScreenSize(window.screen.width)
+    console.log(window.screen.width)
+  }
+
+
 
   const fetchCards = () => {
     fetch(`${API_URL}/card`, {
@@ -39,6 +49,7 @@ export const DashboardForm = () => {
   };
 
   useEffect(() => {
+    screenResize()
     fetchCards();
   }, []);
 
@@ -57,95 +68,94 @@ export const DashboardForm = () => {
   };
 
   return (
-    <div className="flex">
-      <VerticalHeader />
+    <div className={`${Number(screenSize) > 768 ? 'flex w-full' : '' }`} onResize={screenResize}>
+      <div >
+         <VerticalHeader /> 
+      </div>
+      
 
-      <div className="flex-1 p-6">
+      <div className="container mx-auto">
+        <div className="flex-1 p-6">
         <h1 className="text-2xl font-bold mb-2">Dashboard</h1>
         <p className="text-gray-600 mb-6">
           Overview of your financial accounts and activity
         </p>
-
+      
         <FinancialOverview />
-
-        {cardData && cardData.length > 0 && (
-          <div className="flex gap-6 mt-10 flex-wrap">
-            {cardData.map((card, index) => (
-              <div
-                key={index}
-                className="w-80 p-5 border border-gray-300 rounded-xl shadow bg-white cursor-pointer"
-                onClick={() => {
-                  setSelectedCard(card);
-                  setIsDeleteModalOpen(true);
-                }}
-              >
-                <div className="flex justify-between mb-2">
-                  <h2 className="text-lg font-semibold">Card #{index + 1}</h2>
-                  <span className="text-sm text-green-600 font-medium">
-                    Active
-                  </span>
-                </div>
-                <div
-                  className={`rounded-xl h-48 p-4 text-white relative overflow-hidden ${
-                    card.design === "black"
-                      ? "bg-black"
-                      : card.design === "blue-purple"
-                      ? "bg-gradient-to-r from-blue-500 to-purple-600"
-                      : "bg-gradient-to-r from-green-400 to-teal-500"
-                  }`}
+        
+        <div className="flex justify-between items-center pt-8 pb-3">
+              <h3 className="text-2xl font-bold">Your Cards</h3>
+               <button
+                  className="bg-black text-white px-5 py-2.5 rounded-lg border border-black text-sm hover:bg-gray-800"
+                  onClick={() => navigate("/card-create")}
                 >
-                  <FiCreditCard className="absolute top-3 right-3 text-2xl" />
-                  {card.number &&
-                  card.cardholder_name &&
-                  card.cardholder_surname &&
-                  card.expiration_date ? (
-                    <div className="absolute bottom-4 left-4">
-                      <div className="mb-1 text-lg tracking-widest">
-                        **** **** **** {card.number.slice(-4)}
-                      </div>
-                      <div className="text-sm">
-                        {card.cardholder_name} {card.cardholder_surname}
-                      </div>
-                      <div className="text-sm mt-1">
-                        Expires {card.expiration_date}
-                      </div>
-                      <div className="text-sm">CVV ***</div>
-                      <div className="text-sm mt-1 font-semibold">
-                        Balance: ${card.balance.toFixed(2)}
-                      </div>
-                    </div>
-                  ) : (
-                    <div className="absolute bottom-4 left-4 text-sm text-red-500">
-                      Invalid card data
-                    </div>
-                  )}
-                </div>
-              </div>
-            ))}
+                  + Create new
+                </button>
           </div>
-        )}
-
-        <div className="flex justify-center mt-10">
-          <div className="border border-gray-300 rounded-xl p-8 w-full max-w-md bg-white shadow-sm">
-            <div className="flex justify-center mb-5 text-gray-700 text-5xl">
-              <FiCreditCard />
-            </div>
-            <h2 className="text-xl font-semibold text-black text-center mb-2">
-              Apply for a New Card
-            </h2>
-            <p className="text-gray-600 text-center mb-5">
-              Get a new debit or credit card for your account
-            </p>
-            <div className="flex justify-center">
-              <button
-                className="bg-black text-white px-5 py-2.5 rounded-lg border border-black text-sm hover:bg-gray-800"
-                onClick={() => navigate("/card-create")}
-              >
-                + Apply Now
-              </button>
+        <div className="flex ">
+          
+          <div className="flex overflow-x-auto">
+            <div className="flex gap-6 pr-5 w-max">
+              {cardData && cardData.length > 0 && 
+                cardData.map((card, index) => (
+                  <div
+                    key={index}
+                    className="w-90  p-5 border border-gray-300 rounded-xl shadow bg-white cursor-pointer"
+                    onClick={() => {
+                      setSelectedCard(card);
+                      setIsDeleteModalOpen(true);
+                    }}
+                  >
+                    <div className="flex justify-between mb-2">
+                      <h2 className="text-lg font-semibold">Card #{index + 1}</h2>
+                      <span className="text-sm text-green-600 font-medium">Active</span>  
+                    </div>
+                    <div
+                      className={`rounded-xl h-48 p-4 text-white relative overflow-hidden ${
+                        card.design === "black"
+                          ? "bg-black"
+                          : card.design === "blue-purple"
+                          ? "bg-gradient-to-r from-blue-500 to-purple-600"
+                          : "bg-gradient-to-r from-green-400 to-teal-500"
+                      }`}
+                    >
+                      <FiCreditCard className="absolute top-3 right-3 text-2xl" />
+                      {card.number &&
+                      card.cardholder_name &&
+                      card.cardholder_surname &&
+                      card.expiration_date ? (
+                        <div className="absolute bottom-4 left-4">
+                          <div className="mb-1 text-lg tracking-widest">
+                            **** **** **** {card.number.slice(-4)}
+                          </div>
+                          <div className="text-sm">
+                            {card.cardholder_name} {card.cardholder_surname}
+                          </div>
+                          <div className="text-sm mt-1">
+                            Expires {card.expiration_date}
+                          </div>
+                          <div className="text-sm">CVV ***</div>
+                          <div className="text-sm mt-1 font-semibold">
+                            Balance: ${card.balance.toFixed(2)}
+                          </div>
+                        </div>
+                      ) : (
+                        <div className="absolute bottom-4 left-4 text-sm text-red-500">
+                          Invalid card data
+                        </div>
+                      )}
+                    </div>
+                  </div>
+                ))}
             </div>
           </div>
         </div>
+      </div>
+      
+
+       
+
+
 
         {isDeleteModalOpen && selectedCard && (
           <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
